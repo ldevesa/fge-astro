c43e576fbd8ce26d13ae0ef9721c8ba9f6be4ce4
ldevesa <ldevesa@latcom.com>
2024-04-11 11:13:11 -0300

    desinstale vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import alpinejs from "@astrojs/alpinejs";
-import vercel from "@astrojs/vercel/serverless";
+//import vercel from "@astrojs/vercel/serverless";
 
-import react from "@astrojs/react";
+//import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: vercel(),
+  //adapter: vercel(),
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
