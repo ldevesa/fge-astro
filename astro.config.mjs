0251f5731780de3ba0cc75f5c835ee3b95b55b3b
ldevesa <ldevesa@latcom.com>
2024-04-17 17:48:19 -0300

    mas cerca de conseguir cambio de idioma en post collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,8 @@ export default defineConfig({
     defaultLocale: "es",
     locales: ["es", "en"],
     routing: {
-      prefixDefaultLocale: true
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false
     },
     fallback: {
       en: "es"
