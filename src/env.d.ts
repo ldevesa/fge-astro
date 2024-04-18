7cabb15813ff9255c23432f0483127d8ee2ac481
ldevesa <ldevesa@latcom.com>
2024-04-18 16:10:23 -0300

    otra prueba con variables de entorno

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-CONDITION_PATHNAME = Astro.url.pathname === `/es/${nuevaURL}` || Astro.url.pathname === `/en/${nuevaURL}`
-CONDITION_PATHNAME_2 = Astro.url.pathname === `/es/${actualizarURL(lang)}` || Astro.url.pathname === `/en/${actualizarURL(lang)}`
+HOLA = "hola"
