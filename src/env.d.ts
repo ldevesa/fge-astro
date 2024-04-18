654f59cf0ae6db3c6a2911a7928b6968a7aec26c
ldevesa <ldevesa@latcom.com>
2024-04-18 15:09:33 -0300

    probando condicional para vercel

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+CONDITION_PATHNAME = Astro.url.pathname === `/es/${nuevaURL}` || Astro.url.pathname === `/en/${nuevaURL}`
+CONDITION_PATHNAME_2 = Astro.url.pathname === `/es/${actualizarURL(lang)}` || Astro.url.pathname === `/en/${actualizarURL(lang)}`
