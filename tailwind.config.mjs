16480d380c85346ec80ddaf1f1fca645ab7435da
ldevesa <ldevesa@latcom.com>
2024-04-18 11:28:59 -0300

    Se reviso el codigo haciendo un check, y se eliminaron todos los hints

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 
-const withMT = require("@material-tailwind/react/utils/withMT");
+import withMT from "@material-tailwind/react/utils/withMT";
 
-module.exports = withMT({
+export default withMT({
 	content: [	'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',	],
 	theme: {
 		extend: {},
