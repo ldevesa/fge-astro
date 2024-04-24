3adf72ee7c59794278e4b6ea75d44bc6bb841a5d
ldevesa <ldevesa@latcom.com>
2024-04-24 18:03:42 -0300

    saque y puse otras librerias

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-
-import withMT from "@material-tailwind/react/utils/withMT";
-
-export default withMT({
-	content: [	'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',	],
+export default {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js',
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
-});
-
+	plugins: [
+		require("daisyui"),
+		require('flowbite/plugin'),
+	],
+}
